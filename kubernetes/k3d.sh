1abaa9949f46c490f1f8dcfc0a965d95f604164c
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-10-03 14:01:25 +0530

    new

---
+++ kubernetes/k3d.sh
@@ -0,0 +1,7 @@
+kubectl apply -f frontendeployment.yml
+kubectl apply -f frontendservice.yml
+kubectl apply -f backenddeployment.yml
+kubectl apply -f backendservice.yml
+kubectl apply -f mysqldeployment.yml
+kubectl apply -f mysqldeployment.yml
+kubectl apply -f ingress-service.yml
